17f3d55d65af3c6f3a2dfe566bbc05deace3658d
Rob1-alt <robin.pautigny@hetic.net>
2021-06-24 22:29:09 +0200

    new waiting screen

--- src/main.js
+++ src/main.js
@@ -1,15 +1,25 @@
-const mainMenu = document.querySelector('.mainMenu');
-const closeMenu = document.querySelector('.closeMenu');
-const openMenu = document.querySelector('.openMenu');
+const mainMenu = document.querySelector('.mainMenu')
+const closeMenu = document.querySelector('.closeMenu')
+const openMenu = document.querySelector('.openMenu')
+const cursor = document.getElementById('cursor')
 
-openMenu.addEventListener('click',show);
-closeMenu.addEventListener('click',close);
+
+document.addEventListener('mousemove' , function(e){
+    var x = e.clientX
+    var y = e.clientY
+    document.getElementById('cursor').style.left = x - 10 + 'px';
+    document.getElementById('cursor').style.top = y - 10 + 'px';
+
+})
+
+openMenu.addEventListener('click',show)
+closeMenu.addEventListener('click',close)
 
 function show(){
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
+    mainMenu.style.display = 'flex'
+    mainMenu.style.top = '0'
 }
 function close(){
-    mainMenu.style.top = '-200%';
+    mainMenu.style.top = '-200%'
 }
 
